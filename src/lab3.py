78f16c67f5305ee9986cc3adead53b04390b3c13
paopaowu <897873227@qq.com>
2021-06-24 19:20:50 +0800

    Update lab3.py

--- src/lab3.py
+++ src/lab3.py
@@ -1,3 +1,11 @@
 import sys
-for arg in sys.argv:
-    print(arg)
+
+def v():
+    print(sys.version)
+def main():
+    for arg in sys.argv:
+        if arg=="-v":
+            v()
+
+if __name__ == '__main__':
+    main()
