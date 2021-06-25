3f5ee711cec98a0f78d402327078aad8bcedcafb
paopaowu <897873227@qq.com>
2021-06-25 15:29:28 +0800

    Update lab3.py

--- src/lab3.py
+++ src/lab3.py
@@ -1,38 +1,78 @@
 import sys
 import os
+
 def v():
     print(sys.version)
+def decoratorForPath(func,com):
+    def wrapper():
+        if len(com)==1:
+            print("The command requires at least one parameter!")
+        else :
+            func(com[1:])
+    return wrapper
 
-def path(s):
-    print(os.getcwd()+"\\"+s)
-def cat(sc,p1,p2):
+def path(list):
+    for s in list:
+        print(os.getcwd()+"\\"+s)
+
+def decoratorForCat(func,sc,args):
+    def wrapper():
+        if sc not in ["w","c","r"]:
+            print(sc+" is not a valid command!")
+        else :
+            func(sc,args)
+    return wrapper
+
+def cat(sc,args):
     if sc=='w':
-        f1 = open(p1, 'w')
-        f1.write(p2)
+        f1 = open(args[0], 'w')
+        f1.write(args[1])
         f1.close()
     elif sc=="c":
-        f1 = open(p1, "r")
-        f2 = open(p2, "w")
+        f1 = open(args[0], "r")
+        f2 = open(args[1], "w")
         f2.write(f1.read())
         f1.close()
         f2.close()
     elif sc=="r":
-        f1 = open(p1, "r")
+        f1 = open(args[0], "r")
         print(f1.read())
-
         f1.close()
+
+def decoratorForStringToInt(func,s):
+    def wrapper():
+        str_1 = list(s)
+        for i in str_1:
+            if i.isalpha():
+                print("Parameters cannot contain letters!")
+        else :
+            func(s)
+    return wrapper
+
 def stringToInt(s):
     new=int(s)
     print(type(new))
-def main():
-    if sys.argv[1]=="-v":
-        v()
-    elif sys.argv[1]=="-p":
-        path(sys.argv[2])
-    elif sys.argv[1]=="-c":
-        cat(sys.argv[2],sys.argv[3],sys.argv[4])
-    elif sys.argv[1]=="-s":
-        stringToInt(sys.argv[2])
 
+def decoratorForMain(func):
+    def wrapper():
+        global com
+        com=sys.argv[1:]
+        if com[0] not in ["-v","-p","-c","-s"]:
+            print(com[0]+" is not a valid command!")
+        else:
+            func()
+    return wrapper
+
+def main():
+    global com
+    if com[0]=="-v":
+        v()
+    elif com[0]=="-p":
+        decoratorForPath(path,com)()
+    elif com[0]=="-c":
+        decoratorForCat(cat,com[1],com[2:])()
+    elif com[0]=="-s":
+        decoratorForStringToInt(stringToInt,com[1])
+com=[]
 if __name__ == '__main__':
-    main()
+    decoratorForMain(main)()
