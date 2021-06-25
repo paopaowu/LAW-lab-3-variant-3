d6171e8e4f8b94421565b1fc0412a4d84902ba1a
paopaowu <897873227@qq.com>
2021-06-25 10:56:21 +0800

    1

--- src/lab3.py
+++ src/lab3.py
@@ -5,15 +5,21 @@ def v():
 
 def path(s):
     print(os.getcwd()+"\\"+s)
-
-
-
-
-
-
-
-
-
+def cat(sc,p1,p2):
+    if sc=='w':
+        f1 = open(p1, 'w')
+        f1.write(p2)
+        f1.close()
+    elif sc=="c":
+        f1 = open(p1, "r")
+        f2 = open(p2, "w")
+        f2.write(f1.read())
+        f1.close()
+        f2.close()
+    elif sc=="r":
+        f1 = open(p1, "r")
+        print(f1.read())
+        f1.close()
 
 def main():
     for a in sys.argv:
@@ -22,5 +28,8 @@ def main():
         v()
     elif sys.argv[1]=="-p":
         path(sys.argv[2])
+    elif sys.argv[1]=="-c":
+        cat(sys.argv[2],sys.argv[3],sys.argv[4])
+
 if __name__ == '__main__':
     main()
